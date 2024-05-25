b490f6090a4256f78313e7b0ca96501e456d8c4c
gerald <gyoung15000@gmail.com>
2024-05-25 12:37:34 -0600

    intial commit v0.0.1

---
+++ src/app/views/home/components/submit-form/submit-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-submit-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './submit-form.component.html',
+  styleUrl: './submit-form.component.css'
+})
+export class SubmitFormComponent {
+
+}
