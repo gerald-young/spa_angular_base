11c5c54e499c964103e16c87cdfe42b71ef42ae6
gerald <gyoung15000@gmail.com>
2024-05-26 08:48:13 -0600

    footer component v0.1.4

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { SubmitFormComponent } from './components/submit-form/submit-form.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, SubmitFormComponent],
+  imports: [HeaderComponent, SubmitFormComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
